6486ee9808698f149e0d3cf7553fe1b58eaae206
Augusto Wong <augustowong2010@hotmail.com>
2017-12-10 20:58:31 -0500

    added new class and fxml for the question activity/fragment

---
+++ app/src/main/java/com/armz/simplequestions/QuestionFragment.java
@@ -0,0 +1,29 @@
+package com.armz.simplequestions;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by augustowong on 12/10/17.
+ */
+
+public class QuestionFragment extends Fragment {
+    private Question mQuestion;
+
+    private TextView mQuestionTextView;
+    private TextView mAnswerTextView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_question_list, container, false);
+
+        return rootView;
+    }
+
+}
